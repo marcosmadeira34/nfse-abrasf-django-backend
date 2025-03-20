e5d61e08258523ea7686145648305cf9d025417d
Marcos Madeira <mmadeirasilva5@gmail.com>
2025-03-20 10:00:19 -0300

    Configurado Redis em settings.py e Procfile

--- nfse_abrasf/settings.py
+++ nfse_abrasf/settings.py
@@ -161,6 +161,10 @@ STATICFILES_DIRS = [
 INSTALLED_APPS += ['whitenoise.runserver_nostatic']
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
-CELERY_BROKER_URL = os.getenv("REDISCLOUD_URL")
+# Configuração do Celery
+CELERY_BROKER_URL = os.getenv('REDISCLOUD_URL', 'redis://localhost:6379/0')
+
+# Outras configurações do Celery
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
